6731bced7a54c327f544acf662e533a10a3fe10f
dltv1995 <dltv1995@gmail.com>
2026-08-03 14:06:18 +0700

    Add files via upload

---
+++ ar-logo/js/config.js
@@ -0,0 +1,18 @@
+/* แก้ไฟล์นี้เท่านั้น: ลำดับ targetIndex ต้องตรงกับลำดับภาพตอน Compile */
+window.AR_CONFIG = {
+  mindFile: "./targets-all.mind",
+  mindRevision: 1,
+  autoStartCamera: true,
+  foundDelay: 300,
+  redirectDelay: 450
+};
+
+window.AR_TARGETS = [
+  { targetIndex: 0, title: "DLTV", linkUrl: "https://drive.google.com/file/d/1ca0sW0dnE03lvCHtZCTE5ynLQGfx1QRz/view?usp=sharing" },
+  { targetIndex: 1, title: "สพฐ", linkUrl: "https://drive.google.com/file/d/16bFIdBUgPCX3hbTivfhry9mVPNVoTX9L/view?usp=sharing" },
+  { targetIndex: 2, title: "อปท", linkUrl: "https://drive.google.com/file/d/1Dy3r17t--th4kqRXPU-sXoswBJsRjH8Y/view?usp=sharing" },
+  { targetIndex: 3, title: "ตชด", linkUrl: "https://drive.google.com/file/d/1k05FzNplvuT-m0CCUYD6oBa6zwZipFfC/view?usp=sharing" },
+  { targetIndex: 4, title: "พศ", linkUrl: "https://drive.google.com/file/d/1IFLhDmYhDhi5Hp3_yGaMCLJ5HDWkqyCA/view?usp=sharing" },
+  { targetIndex: 5, title: "สช", linkUrl: "https://drive.google.com/file/d/1RrhXe_-SzBTob9u1FaBtVxVZmFIW3ZR5/view?usp=sharing" },
+  { targetIndex: 6, title: "กศน", linkUrl: "https://drive.google.com/file/d/1J1DDB3Ah7mmZangvwo0oq_sHQDuq1c1g/view?usp=sharing" }
+];
